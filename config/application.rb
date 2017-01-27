09d25d770561146ac696a5fe1e8d00f67021c158
Łukasz Korol <lukas.korol@gmail.com>
2017-01-27 19:48:28 +0100

    Configured dot-env initializer and hided values in secret.yml

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
-
+require 'dotenv' ; Dotenv.load ".env.local", ".env.#{Rails.env}"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
